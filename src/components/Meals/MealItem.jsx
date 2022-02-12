b911ae5da02d819be34147fef761874a5f245395
nachbr91 <nabero91@gmail.com>
2022-02-12 17:47:08 +0100

    Edit AvailableMeals component adding MealItem component

---
+++ src/components/Meals/MealItem.jsx
@@ -0,0 +1,20 @@
+import styles from './MealItem.module.css'
+
+const MealItem = (props) => {
+  const price = `${props.price.toFixed(2)} €`
+
+  return (
+    <li className={styles.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={styles.description}>{props.description}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+      <div>
+        <form action=""></form>
+      </div>
+    </li>
+  )
+}
+
+export default MealItem
