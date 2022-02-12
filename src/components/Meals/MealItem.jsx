69c3dfdda65e71a887259360413a7028f33e0a5f
nachbr91 <nabero91@gmail.com>
2022-02-12 18:07:18 +0100

    Add MealItemForm and Input components. Edit MealItem component

--- src/components/Meals/MealItem.jsx
+++ src/components/Meals/MealItem.jsx
@@ -1,4 +1,5 @@
 import styles from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 
 const MealItem = (props) => {
   const price = `${props.price.toFixed(2)} €`
@@ -11,7 +12,7 @@ const MealItem = (props) => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-        <form action=""></form>
+        <MealItemForm />
       </div>
     </li>
   )
